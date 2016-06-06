c41c28c6d5993b20b52a87fb5556ca310b99cc2e
kelseym <kelseym@mir.wustl.edu>
2016-06-05 19:32:44 -0500

    Totally fixed it this time.

--- pydicom_example/pydicom.py
+++ pydicom_example/pydicom.py
@@ -3,12 +3,11 @@
 # Converts a directory of one or more DICOM files into the NIfTI format
 # Primary use is to test the XNAT Docker service.
 # Input directory is assumed to be /input
-#   Input directory format is assumed to be /input/SCANS/{scan_id}/DICOM/
-# Output directory is assumed to be /output
-#   Output directory format will be generated as /output/SCANS/{scan_id}/NIFTI/
+#   Input directory format is assumed to be /data/input/*.dcm
+# Output directory is assumed to be /data/output
+#   Output directory format will be generated as /data/output/data.nii.gz
 
-
-# USAGE: python pyscript.py 
+# USAGE: python pyscript.py
 
 import sys
 import os
@@ -23,24 +22,10 @@ def main(argv):
     output_dir = '/output'
 
     # parse input directory structure by scan id
-    for scan, dcm in get_scan_sets(input_dir).iteritems():
-        # use dcmstack to create a nifti file from each scan set
-        nifti_file_name = os.path.join(output_dir, scan + '.nii.gz');
-        dcm_to_nii(dcm, nifti_file_name)
-
-
-def get_scan_sets(input_dir):
-    scans = {}
-    dcm_base_folder = os.path.join(input_dir, 'SCANS')
-    if os.path.exists(dcm_base_folder):
-        for scan_dir in os.listdir(dcm_base_folder):
-            if os.path.isdir(os.path.join(dcm_base_folder, scan_dir)):
-                scan = os.path.basename(scan_dir)
-                dcm = glob(os.path.join(dcm_base_folder, scan_dir, 'DICOM', '*.dcm'))
-                scans[scan] = dcm
-    else:
-        print(dcm_base_folder + ' does not exist.')
-    return scans
+    dcm = glob(input_dir + os.path.sep + '*.dcm')
+    # use dcmstack to create a nifti file from each scan set
+    nifit_file_name = output_dir + os.path.sep + 'scan' + '.nii.gz';
+    dcm_to_nii(dcm, nifit_file_name)
 
 
 def dcm_to_nii(dcm, nii_file_name):
@@ -56,4 +41,3 @@ def dcm_to_nii(dcm, nii_file_name):
 
 if __name__ == "__main__":
     main(sys.argv[1:])
-
